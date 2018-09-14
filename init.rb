1c3e2be1c9d8cadccbff3c8a7d92067a56f6de56
Hydrant <zhangqiuyun@infohold.com.cn>
2018-09-14 13:31:28 +0800

    加入注册是否可用的控制

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_omniauth_gitlab do
   name 'Redmine Omniauth Gitlab Plugin'
   author 'zhangqiuyun@infohold.com.cn'
   description 'This is a plugin for Redmine registration through gitlab'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/qbcbyb/redmine_omniauth_gitlab'
   author_url 'https://github.com/qbcbyb'
 
@@ -13,7 +13,14 @@ Redmine::Plugin.register :redmine_omniauth_gitlab do
     :site => "",
     :client_id => "",
     :client_secret => "",
-    :oauth_autentification => false,
-    :allowed_domains => ""
+    :oauth_authentification => false,
+    :allowed_domains => "",
+    :register_enable => false
   }, :partial => 'settings/gitlab_settings'
+
+  Redmine::MenuManager.items(:account_menu).root.children.each do |node|
+    if node.name == :register 
+      node.@condition = Proc.new { !User.current.logged? && Setting.self_registration? && !Setting.plugin_redmine_omniauth_gitlab['register_enable'] }
+    end
+  end
 end
